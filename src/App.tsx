99b97c24b3a680261468cbc094ccfd8b1db413de
viniciuslft <viniciuspedrozaestrela@gmail.com>
2025-11-25 22:02:12 -0300

    add: menu + padronização

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Heading } from './components/Heading';
-
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <>
-      <Heading>Olá mundo 1</Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
-        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
-        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
-        quia rerum!
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
     </>
   );
 }
