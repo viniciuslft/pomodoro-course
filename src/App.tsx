a5b1e1f54578685cdcf1a05a4aa1e95e30fe3afe
viniciuslft <viniciuspedrozaestrela@gmail.com>
2025-11-25 22:05:24 -0300

    add: CountDown component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -13,6 +14,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
