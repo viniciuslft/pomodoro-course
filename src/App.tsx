a935cca217bfe0d8c72b738be09891b29dc17ce8
viniciuslft <vinicius.pedroza@yeb.com.br>
2025-11-26 12:57:43 -0300

    add: DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -18,6 +19,27 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
