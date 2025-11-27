112111ec230a76453f6278a4849be692e11be85c
viniciuslft <vinicius.pedroza@yeb.com.br>
2025-11-27 13:13:19 -0300

    add: Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
