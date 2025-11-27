140f7004301c4cd48de2da368224cb999665fd0d
viniciuslft <vinicius.pedroza@yeb.com.br>
2025-11-26 23:14:16 -0300

    add: GenericHTML + TaskModel + Home + NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
+import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
 
 export function App() {
-  const [numero, setNumero] = useState(0);
-  return (
-    <>
-      <Heading>{numero}</Heading>
-      <button onClick={() => setNumero(prevState => prevState + 1)}>
-        Aumenta
-      </button>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText={numero.toString()}
-              id='meuInput'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
